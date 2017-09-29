daf5d4632c0222dcb5e11dfb0f2219dba3b37122
StephenCurry30 <1126078708@qq.com>
2017-09-29 19:39:00 +0800

    测试

--- CoreNFCDemo01/ViewController.swift
+++ CoreNFCDemo01/ViewController.swift
@@ -7,13 +7,48 @@
 //
 
 import UIKit
+import CoreNFC
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, NFCNDEFReaderSessionDelegate {
+    
+    // MARK: Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+//        if UIDevice.current.systemVersion {
+//
+//        }
+        if NFCNDEFReaderSession.readingAvailable {
+            let session = NFCNDEFReaderSession.init(delegate: self, queue: nil, invalidateAfterFirstRead: true)
+            session.begin()
+        }
     }
+    
+    // MARK: NFCNDEFReaderSessionDelegate
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        print("出错了：", error.localizedDescription)
+    }
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        for message in messages {
+            for playLoad in message.records {
+                print("typeNameFormat: ", playLoad.typeNameFormat)
+                print("type: ", playLoad.type)
+                print("identifier: ", playLoad.identifier)
+                print("payload: ", playLoad.payload)
+                guard playLoad.payload.count != 0 else {
+                    return
+                }
+                let str: NSString = NSString(data: playLoad.payload, encoding: String.Encoding.utf8.rawValue)!
+                print("payloadStr: ", str)
+            }
+        }
+    }
+    
+    // MARK: Event response
+    
+    // MARK: -
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
