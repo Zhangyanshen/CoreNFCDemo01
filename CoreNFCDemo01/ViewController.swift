ee1d92e95461daf7603b1b77d6aae205fc9b94a0
StephenCurry30 <1126078708@qq.com>
2017-09-29 10:18:38 +0800

    Initial Commit

---
+++ CoreNFCDemo01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreNFCDemo01
+//
+//  Created by 张延深 on 2017/9/29.
+//  Copyright © 2017年 张延深. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
